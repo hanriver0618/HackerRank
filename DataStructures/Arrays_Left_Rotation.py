b23ea3caea86d6b25c708a4ca4c2c09b74712e1c
Mihwa <hanriver0618@gmail.com>
2017-09-04 10:18:04 -0400

    Arrays

---
+++ DataStructures/Arrays_Left_Rotation.py
@@ -0,0 +1,13 @@
+import sys
+
+def leftRotation(a, d):
+    # Complete this function
+    return a[d:] + a[:d]
+
+if __name__ == "__main__":
+    n, d = input().strip().split(' ')
+    n, d = [int(n), int(d)]
+    a = list(map(int, input().strip().split(' ')))
+    result = leftRotation(a, d)
+    print (" ".join(map(str, result)))
+
